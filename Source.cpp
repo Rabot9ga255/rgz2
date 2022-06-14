f064e0a94a8c08faf46176ecbadca6f2651663c1
Rabot9ga255 <60039557+Rabot9ga255@users.noreply.github.com>
2022-06-14 16:25:51 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,77 +1,72 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <Windows.h>
+#include <iostream> 
+#include <fstream> 
+#include <string> 
+#include <Windows.h> 
+
 
 using namespace std;
 
 
-class Address {
-private:
-    string locality;            // населений пункт
-    string street;              // вулиця
-    string house_number;        // номер будинку
-    string apartment_number;    // номер квартири
 
-public:
 
-    void setData(string loc, string str, string house, string apartment) {
-        locality = loc;
-        street = str;
-        house_number = house;
-        apartment_number = apartment;
-    }
-
-    void printData() {
-        cout << "Населенний пункт: " << locality << endl;
-        cout << "Вулица: " << street << endl;
-        cout << "Номер будинку: " << house_number << endl;
-        cout << "Номер квартири: " << apartment_number << endl;
-    }
-
-    /*Address() {
-        locality = ""; street = ""; house_number = ""; apartment_number = "";
-    }*/
-
-};
-
-class Human {
-public:
-
-    string name;
-    string surname;
-    string telephone;
-    Address address;
+struct TAGdata
+{
+    char tag[3];        // -128 
+    char title[30];     // -125 
+    char artist[30];    // -95 
+    char album[30];     // -65 
+    char year[4];       // -35 
+    char comment[30];     // -31 
 
 };
 
 
+void clearData(fstream &mp3File, int const mp3TagSize) {
 
-class member_of_social_network : public Human {
-public:
+    string s; string s1; string s2; string s3; string s4;
 
-    string date_registration;
-    int count_of_contacts;      
+    s = s1 = s2 = s4 = "                            ";
+    s3 = "    ";
 
+    mp3File << s;
+    mp3File.seekg(-(mp3TagSize - 33), ios::end);
+    mp3File << s1;
+    mp3File.seekg(-(mp3TagSize - 63), ios::end);
+    mp3File << s2;
+    mp3File.seekg(-(mp3TagSize - 93), ios::end);
+    mp3File << s3;
+    mp3File.seekg(-(mp3TagSize - 97), ios::end);
+    mp3File << s4;
 
-    member_of_social_network() {
-        count_of_contacts = 0;
-    }
-
-};
-
-
-
-class member_of_conference : public Human {
-public:
-
-    string name_report;
-    string date_report;
-    string time_report;
-
-};
+    mp3File.seekg(-(mp3TagSize - 3), ios::end);
+}
 
 
+void fillData(fstream& mp3File, string s, string s1, string s2, string s3, string s4, int const mp3TagSize) {
+
+    mp3File << s;
+    mp3File.seekg(-(mp3TagSize - 33), ios::end);
+    mp3File << s1;
+    mp3File.seekg(-(mp3TagSize - 63), ios::end);
+    mp3File << s2;
+    mp3File.seekg(-(mp3TagSize - 93), ios::end);
+    mp3File << s3;
+    mp3File.seekg(-(mp3TagSize - 97), ios::end);
+    mp3File << s4;
+
+}
+
+
+void printData(TAGdata& tagStruct) {
+
+    printf("\n\nTag: %0.3s\n", tagStruct.tag);
+    printf("Title: %0.30s\n", tagStruct.title);
+    printf("Artist: %0.30s\n", tagStruct.artist);
+    printf("Album: %0.30s\n", tagStruct.album);
+    printf("Year: %0.4s\n", tagStruct.year);
+    printf("Comment: %0.30s\n", tagStruct.comment);
+
+}
 
 
 int main()
@@ -79,42 +74,94 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
+    int const mp3TagSize = 128;
+    TAGdata tagStruct = { 0 };
 
-
-    member_of_social_network social1;
-    member_of_conference conference1;
-
-
-
-    social1.name = "Василь";
-    social1.surname = "Хімочка";
-    social1.telephone = "+380505419172";
-    social1.address.setData("Запоріжжя", "Півночнокільцева", "15а", "82");
-
-    social1.date_registration = "04.06.2022";
-    social1.count_of_contacts = 120;
-
-    // Вивід
-    cout << "\t Робітник соціальної мережі:\n" << social1.name << endl << social1.surname << endl << social1.telephone << endl;
-    social1.address.printData();
-    cout << social1.date_registration << endl << social1.count_of_contacts << endl << endl;
+    fstream mp3File;
 
 
 
+    string filename = "zvuk.mp3";
 
-    conference1.name = "Вікторія";
-    conference1.surname = "Цапко";
-    conference1.telephone = "+380975221523";
-    conference1.address.setData("Запоріжжя", "Кінотеатр космос", "12", "43");
+    mp3File.open(filename);
 
-    conference1.name_report = "Спадкування";
-    conference1.date_report = "10.06.2022";
-    conference1.time_report = "11:30";
 
-    // Вивід
-    cout << "\t Робітник соціальної мережі:\n" << conference1.name << endl << conference1.surname << endl << conference1.telephone << endl;
-    conference1.address.printData();
-    cout << conference1.name_report << endl << conference1.date_report << endl << conference1.time_report << endl << endl;
+    if (mp3File) {
+        cout << "Successful open of " << filename << endl;
+    }
+    else {
+        cerr << "Aborting Could not open " << filename << endl;
+        return 1;
+    }
 
+
+    cout << "\t Inctruction:\n";
+    cout << "write 1 to check ID3 tags\n";
+    cout << "write 2 to change ID3 tags\n";
+    cout << "write 0 to exit\n\n";
+
+    int answer = 50;
+    while (answer != 0) {
+        cin >> answer;
+        if (answer == 1) {
+
+            //move file pointer to 128 bytes off the end of the file. 
+            mp3File.seekg(-(mp3TagSize), ios::end);
+            if (mp3File.fail()) {
+                cerr << "Could not seek, Aborting " << endl;
+                return 1;
+            }
+
+            //get the data 
+            mp3File.read(tagStruct.tag, 3);
+            mp3File.read(tagStruct.title, 30);
+            mp3File.read(tagStruct.artist, 30);
+            mp3File.read(tagStruct.album, 30);
+            mp3File.read(tagStruct.year, 4);
+            /*mp3File.read(tagStruct.comment, 25);*/
+            mp3File.read(tagStruct.comment, 30);
+
+            mp3File.close();
+
+            printData(tagStruct);
+
+            mp3File.open(filename);
+        }
+        else if (answer == 2) {
+
+            //move file pointer to 125 bytes off the end of the file, skip TAG 
+            mp3File.seekg(-(mp3TagSize - 3), ios::end);
+            if (mp3File.fail()) {
+                cerr << "Could not seek, Aborting " << endl;
+                return 1;
+            }
+
+            string s; string s1; string s2; string s3; string s4;
+
+            cout << "Enter title\n";
+            cin.ignore();
+            getline(cin, s);
+            cout << "\nEnter artist\n";
+            getline(cin, s1);
+            cout << "\nEnter album\n";
+            getline(cin, s2);
+            cout << "\nEnter year\n";
+            getline(cin, s3);
+            cout << "\nEnter comment\n";
+            getline(cin, s4);
+
+            clearData(mp3File, mp3TagSize);
+
+            fillData(mp3File, s, s1, s2, s3, s4, mp3TagSize);
+
+
+            mp3File.close();
+            mp3File.open(filename);
+        }
+        cout << "something else?\n\n";
+    }
+
+
+    mp3File.close();
     return 0;
 }
